1f045a48b92304c640592095d3c382af0ab140d8
danisolation <tranquocdungb4@gmail.com>
2026-08-25 13:47:14 +0700

    feat: Add CORS configuration
    - Allow FE at localhost:3001 to call API

--- landing-page-be/src/main.ts
+++ landing-page-be/src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';  // ← Import
+import { ValidationPipe } from '@nestjs/common'; // ← Import
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,              // Bỏ field không có trong DTO
-    forbidNonWhitelisted: true,   // Báo lỗi nếu có field lạ
-  }));
+  app.enableCors({
+    origin: 'http://localhost:3001', // FE được phép gọi
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Bỏ field không có trong DTO
+      forbidNonWhitelisted: true, // Báo lỗi nếu có field lạ
+    }),
+  );
 
   await app.listen(3000);
 }
